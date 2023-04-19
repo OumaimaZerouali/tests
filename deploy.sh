38666ed305d95c36fcd073c22c90e679d30b10cf
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-04-19 15:23:46 +0200

    deploy login docker update again

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 
 echo "Authenticating and pushing image to Docker Hub"
-docker login
+cat ~/secret.txt | docker login --username foo --password-stdin
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker push "${IMAGE_NAME}:latest"
