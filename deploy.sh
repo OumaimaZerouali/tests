972c370e530995b8fc76d42bc48e6c0500ce2624
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-04-19 14:22:00 +0200

    to docker

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,8 @@ IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current comm
 echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
 docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
+
+echo "Authenticating and pushing image to Docker Hub"
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker push "${IMAGE_NAME}:latest"
