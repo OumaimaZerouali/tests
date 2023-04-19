88bc993b12701279cb990fa1e2b1458f2b325540
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-04-19 13:28:12 +0200

    First commit

---
+++ src/test/java/be/ordina/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ordina.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
