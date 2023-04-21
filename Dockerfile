2276938d64b7e6f113e4f257b69c56fd6a6b1ad7
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-04-21 12:31:44 +0200

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:17-jdk-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} test.jar
+ARG JAR_FILE=target/test-0.0.1-SNAPSHOT.jar
+COPY --from=0 ${JAR_FILE} test.jar
 ENTRYPOINT ["java", "-jar", "/test.jar"]
 EXPOSE 8080
 CMD ["java", "-jar", "/test.jar"]
-
--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,6 @@ docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" --build-arg CACHEBUST=$(date +%s) .
 #docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 
-echo "Copying JAR file to target folder"
-mkdir -p target
-cp $HOME/project/target/*.jar target/test.jar
-
 echo "Authenticating and pushing image to Docker Hub"
 docker login --username redwizardofoz --password $DOCKERHUB_PASSWORD
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
