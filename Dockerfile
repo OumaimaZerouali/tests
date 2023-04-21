7a3ae845a69feb8f7e3d482ecc42b832b6e21e3d
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-04-21 12:24:52 +0200

    cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,7 @@
-FROM openjdk:11-slim-buster as build
-COPY .mvn .mvn
-COPY mvnw .
-COPY pom.xml .
-COPY src src
-RUN chmod +x mvnw
-
-
 FROM openjdk:17-jdk-alpine
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} test.jar
 ENTRYPOINT ["java", "-jar", "/test.jar"]
 EXPOSE 8080
 CMD ["java", "-jar", "/test.jar"]
-
 
--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 IMAGE_NAME="redwizardofoz/project_test"
 IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
 
-
-
 echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
 docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" --build-arg CACHEBUST=$(date +%s) .
 #docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
