fbe3ee52764f4e190bb422ec71ee5e0c91feb8b7
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-04-21 11:55:26 +0200

    clear image cache

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ IMAGE_NAME="redwizardofoz/project_test"
 IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
 
 echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
-docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" --build-arg CACHEBUST=$(date +%s) .
+#docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 
 echo "Authenticating and pushing image to Docker Hub"
